7bfde98abef1dfc8b3850d203b192e669448e9df
BCcore2056 <145463812+BCcore2056@users.noreply.github.com>
2023-11-28 15:24:47 +0000

    28.11.2023.v1

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,23 @@
 
 <main>
     <!-- Struktura strony -->
+    <table>
+        <tr>
+            <td class="cell">
+                o
+            </td>
+        </tr>
+    </table>
 </main>
 
 <style>
     /* Styl strony */
+    .cell {
+        width: 100px;
+        height: 100px;
+        background-color: gray;
+        color: balck;
+        text-align: center;
+        font-size: 40px;
+    }
 </style>
